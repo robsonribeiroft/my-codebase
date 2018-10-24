a60a41317f2132dbe881b557c3c44f2008fefbf9
Robson Ribeiro <robsonribeiroft@gmail.com>
2018-10-24 10:31:08 -0300

    Add function to uppercase string

--- c/strings/words/count.c
+++ c/strings/words/count.c
@@ -60,3 +60,13 @@ int countWords(char * string)
 
   return n_words;
 }
+
+//Upcaser strings
+void upper(char str[]){
+    int i=0;
+    while(str[i]){
+        if(str[i]>96 && str[i]<123)
+            str[i] -= 32;
+        i++;
+    }
+}
